7c1b3b4fbfc88da0f5eba0cfce2bbd0897dc1d96
Ryang Sohn <loop.infinitely@gmail.com>
2021-05-23 12:42:29 +0900

    Refactor integral attack code

--- block-cipher/prince_integral_attack.py
+++ block-cipher/prince_integral_attack.py
@@ -9,49 +9,53 @@ import reduced_prince_cipher
 def get_nibble(bitstr, nibble_pos):
     return bitstr[nibble_pos * 4:nibble_pos * 4 + 4]
 
-# pre-process data.
-plaintext_file = ConstBitStream(filename='./data/pt.dat')
-ciphertext_file = ConstBitStream(filename='./data/ct.dat')
-plaintext_set = []
-ciphertext_set = []
-active_nibble_pos = []
-for _ in range(0x500):
-    plaintexts = []
-    ciphertexts = []
+def k1_xor_k0_prime_candidate(ciphertext_set):
+    candidates = []
     for _ in range(16):
-        plaintexts.append(BitArray(plaintext_file.read(64)))
-        ciphertexts.append(BitArray(ciphertext_file.read(64)))
-    plaintext_set.append(plaintexts)
-    ciphertext_set.append(ciphertexts)
-    diff = plaintexts[0] ^ plaintexts[1]
-    pos = diff.find('0b1')[0] % 4
-    active_nibble_pos.append(pos)
-
-# get k1 ^ k0'
-candidates = []
-for _ in range(16):
-    candidates.append({'ok': set(), 'fail': set()})
-
-tries = 5
-for nibble_pos in range(16):
-    for guess_value in range(16):
-        for ciphertexts in ciphertext_set[:tries]:
+        candidates.append({'ok': set(), 'fail': set()})
+    for nibble_pos in range(16):
+        for guess_value in range(16):
             guess = BitArray(hex(guess_value))
             RC3_nibble = get_nibble(reduced_prince_cipher.RC[3], nibble_pos)
-            xor_sum = BitArray(4)
-            for ciphertext in ciphertexts:
-                ciphertext_nibble = get_nibble(ciphertext, nibble_pos)
-                xor_sum ^= reduced_prince_cipher.s_layer(
-                    ciphertext_nibble ^ guess ^ RC3_nibble
-                )
-            if xor_sum.uint == 0 and \
-                guess_value not in candidates[nibble_pos]['fail']:
-                candidates[nibble_pos]['ok'].add(guess_value)
-            elif xor_sum.uint != 0:
-                if guess_value in candidates[nibble_pos]['ok']:
-                    candidates[nibble_pos]['ok'].remove(guess_value)
-                candidates[nibble_pos]['fail'].add(guess_value)
-assert all([len(x['ok']) == 1 for x in candidates])
-k1_xor_k0_prime = reduce(add,
-                         [BitArray(hex(x['ok'].pop())) for x in candidates])
-print(f'k1 ^ k0\' = 0x{k1_xor_k0_prime.hex}')
+            for ciphertexts in ciphertext_set:
+                xor_sum = BitArray(4)
+                for ciphertext in ciphertexts:
+                    ciphertext_nibble = get_nibble(ciphertext, nibble_pos)
+                    xor_sum ^= reduced_prince_cipher.s_layer(
+                        ciphertext_nibble ^ guess ^ RC3_nibble
+                    )
+                if xor_sum.uint == 0 and \
+                    guess_value not in candidates[nibble_pos]['fail']:
+                    candidates[nibble_pos]['ok'].add(guess_value)
+                elif xor_sum.uint != 0:
+                    if guess_value in candidates[nibble_pos]['ok']:
+                        candidates[nibble_pos]['ok'].remove(guess_value)
+                    candidates[nibble_pos]['fail'].add(guess_value)
+    return candidates
+
+def get_result_from_candidate(candidate):
+    assert all([len(x['ok']) for x in candidate])
+    return reduce(add, [BitArray(hex(x['ok'].pop())) for x in candidate])
+
+def read_data_files(plaintext_path='./data/pt.dat',
+                    ciphertext_path='./data/ct.dat'):
+    plaintext_file = ConstBitStream(filename=plaintext_path)
+    ciphertext_file = ConstBitStream(filename=ciphertext_path)
+    plaintext_set = []
+    ciphertext_set = []
+    for _ in range(0x500):
+        plaintexts = []
+        ciphertexts = []
+        for _ in range(16):
+            plaintexts.append(BitArray(uint=plaintext_file.read('uintle:64'), length=64))
+            ciphertexts.append(BitArray(uint=ciphertext_file.read('uintle:64'), length=64))
+        plaintext_set.append(plaintexts)
+        ciphertext_set.append(ciphertexts)
+    return plaintext_set, ciphertext_set
+
+if __name__ == '__main__':
+    # pre-process data.
+    plaintext_set, ciphertext_set = read_data_files()
+    candidates = k1_xor_k0_prime_candidate(ciphertext_set[:5])
+    k1_xor_k0_prime = get_result_from_candidate(candidates)
+    print(f'k1 ^ k0\' = 0b{k1_xor_k0_prime.bin}')
