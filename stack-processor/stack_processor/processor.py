c12e301689076620dd8ecf858e1482f59413ede1
Ryang Sohn <loop.infinitely@gmail.com>
2021-07-17 18:36:51 +0900

    Set __parse_data to be static method

--- stack-processor/stack_processor/processor.py
+++ stack-processor/stack_processor/processor.py
@@ -1,6 +1,7 @@
 from collections import deque
 from typing import List
 
+
 class StackProcessor:
     def __init__(self, data: List[str]):
         self.data = data
@@ -15,13 +16,14 @@ class StackProcessor:
         op2 = self.stack.pop()
         self.stack.append(op1 == op2)
 
-    def __parse_data(self, data: str):
+    @staticmethod
+    def __parse_data(data: str):
         try:
             parsed = int(data)
             return parsed
         except ValueError:
             pass
-        if data[:2] == '0x':
+        if data[:2] == "0x":
             try:
                 parsed = int(data[2:], 16)
                 return parsed
@@ -36,6 +38,6 @@ class StackProcessor:
             if type(parsed) == int:
                 self.stack.append(parsed)
             else:
-                cmd_table = {'ADD': self.__add, 'EQUAL': self.__equal}
-                cmd_table[parsed]() # type: ignore
+                cmd_table = {"ADD": self.__add, "EQUAL": self.__equal}
+                cmd_table[parsed]()  # type: ignore
         return self.stack
