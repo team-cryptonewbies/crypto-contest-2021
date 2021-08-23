70961e502c038927854e0517bb494d80eb64861b
Ryang Sohn <loop.infinitely@gmail.com>
2021-08-24 02:21:22 +0900

    [stack-processor] Add command line argument parser

--- stack-processor/stack_processor/__main__.py
+++ stack-processor/stack_processor/__main__.py
@@ -1,11 +1,16 @@
-import sys
+import argparse
 from .processor import StackProcessor
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Stack processor powered by Team Crypt0newbies"
+    )
+    parser.add_argument("filepath", type=str, help="a stack program file to run")
+    parser.add_argument("--verbose", action="store_true")
+    args = parser.parse_args()
     data = []
-    with open(sys.argv[1]) as f:
-        for lines in f:
-            for word in lines.split():
-                data.append(word)
-    processor = StackProcessor(data)
+    with open(args.filepath) as f:
+        for line in f:
+            data.append(line.strip())
+    processor = StackProcessor(data, verbose=args.verbose)
     print(processor.run())
